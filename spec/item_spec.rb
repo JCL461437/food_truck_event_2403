2a31af70451287166aa1d6990075004667ba1191
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-15 09:17:52 -0600

    Add final tests for Item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,16 +7,21 @@ end
 
 RSpec.describe Item do
     before(:each) do
-        @item = Item.new
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
 
     describe '#initialize' do
         it 'can initialize' do
-            expect(@item).to be_an_instance_of(Item)
+            expect(@item1).to be_an_instance_of(Item)
         end
         
-        it 'can collect all cells' do
-            expect(@item.).to eq)
+        it 'can return item name' do
+            expect(@item1.name).to eq("Peach Pie (Slice)")
+        end
+
+        it 'can return item price' do
+            expect(@item1.price).to eq("$3.75")
         end
     end
 
