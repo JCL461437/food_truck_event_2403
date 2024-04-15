623ecac6ce1cdc5d16bb4ec7c41c99bc6627d969
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-15 11:53:50 -0600

    Add refactored methods and potential_revenue method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -10,12 +10,32 @@ class FoodTruck
     
     end
 
+    # def check_stock(item)
+    #     @inventory.count(item)
+    # end
+
+    # def stock(item, quantity)
+    #     @inventory.store(item, quantity)
+    # end
+
     def check_stock(item)
-        @inventory.count(item)
+        @inventory[item] || 0
+    end
+    
+    def stock(item, quantity)
+        @inventory[item] = quantity
+    end
+    
+    def potential_revenue
+        total_revenue = Float.new()
+        
+        @inventory.each do |item, quantity|
+            total_revenue += (item.price.to_f * quantity)
+        end
+        
+        total_revenue
     end
 
-    def stock(item, quantity)
-        @inventory.store(item, quantity)
-    end
+
 end
 
