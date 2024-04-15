c48d6073bfff9b40a3a82ce301b50a866c5dcd29
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-15 11:03:06 -0600

    Add the food_trucks_that_sell method to Event Class

--- lib/event.rb
+++ lib/event.rb
@@ -18,12 +18,17 @@ class Event
         @food_trucks.map do |food_truck|
             food_truck.name
         end
-        
     end
 
-    # def 
-    # end
 
-    # def 
-    # end
+    def food_trucks_that_sell(item)
+        food_trucks_selling_item = []
+        @food_trucks.each do |food_truck|
+            if food_truck.inventory.keys.any? { |inventory_item| inventory_item.name == item.name }
+                food_trucks_selling_item << food_truck
+            end   
+        end
+        food_trucks_selling_item
+    end
+    
 end
