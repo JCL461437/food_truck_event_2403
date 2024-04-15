33e2f738087d0f3a4dd2e9f3210260be46e8b574
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-15 09:38:44 -0600

    Add stock and check_stock method tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -8,6 +8,8 @@ end
 RSpec.describe FoodTruck do
     before(:each) do
         @food_truck = FoodTruck.new("Rocky Mountain Pies")
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
 
     describe '#initialize' do
@@ -16,7 +18,7 @@ RSpec.describe FoodTruck do
         end
 
         it 'can return food truck name' do
-            expect(@food_truck.name).to eq("Rocky Mountain Pires")
+            expect(@food_truck.name).to eq("Rocky Mountain Pies")
         end
         
         it 'can return food truck inventory' do
@@ -26,8 +28,16 @@ RSpec.describe FoodTruck do
 
     describe '#check_stock' do
         it 'can check stock for an item' do
-            expect(@food_truck.check_stock).to eq(0)
+            expect(@food_truck.check_stock(@item1)).to eq(0)
         end
     end
+
+    describe '#stock' do
+        it 'can stock an item' do
+            @food_truck.stock(@item1, 30)
+            expect(@food_truck.inventory).to eq({@item1 => 30})
+        end
+    end
+
 
 end
